9e0798fef18e85a8b7e34766d63a3f7ae31d76a5
Clar1fy <timplifier@gmail.com>
2022-07-17 00:24:49 +0600

    fix(OnboardFragment.kt): delete useless listener to navigate

--- app/src/main/java/com/geektech/beauty/presentation/ui/fragments/authentication/onboard/OnboardFragment.kt
+++ app/src/main/java/com/geektech/beauty/presentation/ui/fragments/authentication/onboard/OnboardFragment.kt
@@ -4,8 +4,6 @@ import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.beauty.R
 import com.geektech.beauty.core.base.BaseFragment
-import com.geektech.beauty.core.extensions.mainNavController
-import com.geektech.beauty.core.extensions.navigateSafely
 import com.geektech.beauty.data.local.preferences.AuthenticationPreferencesManager
 import com.geektech.beauty.databinding.FragmentOnboardBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,10 +17,5 @@ class OnboardFragment :
     lateinit var authenticationPreferencesManager: AuthenticationPreferencesManager
     override val binding by viewBinding(FragmentOnboardBinding::bind)
     override val viewModel by viewModels<OnboardViewModel>()
-    override fun performListeners() {
-        binding.tv.setOnClickListener {
-            authenticationPreferencesManager.isAuthenticated = true
-            mainNavController().navigateSafely(R.id.action_authenticationFlowFragment_to_mainFlowFragment)
-        }
-    }
+
 }
