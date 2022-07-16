9d54b83cd8eaebbe01e9299c68131e3a171def1a
Clar1fy <timplifier@gmail.com>
2022-07-17 03:18:23 +0600

    feat(appSelection): instantiated ChooseCustomerOrSalonFragment,its viewModel and layout; imported inter_medium & inter_regular fonts

---
+++ app/src/main/java/com/geektech/beauty/presentation/ui/fragments/authentication/appSelection/ChooseCustomerOrSalonViewModel.kt
@@ -0,0 +1,8 @@
+package com.geektech.beauty.presentation.ui.fragments.authentication.appSelection
+
+import com.geektech.beauty.core.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ChooseCustomerOrSalonViewModel  : BaseViewModel(){
+}
