fc4a5a509eb1d0638ff4d8d424152bcccba93dc6
Clar1fy <timplifier@gmail.com>
2022-07-17 23:30:25 +0600

    added one more parameter to the DetailedCategoryUI.kt - serviceImage

--- app/src/main/java/com/geektech/beauty/presentation/models/DetailedCategoryUI.kt
+++ app/src/main/java/com/geektech/beauty/presentation/models/DetailedCategoryUI.kt
@@ -1,6 +1,7 @@
 package com.geektech.beauty.presentation.models
 
 data class DetailedCategoryUI(
+    val serviceImage: String,
     val serviceType: String,
     val serviceTime: String,
     val serviceCost: String
