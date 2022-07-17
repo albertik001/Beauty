31cbaf292fc3e25918df11fa64699b38411705f7
albertik001 <albert.nurlanovv@mail.ru>
2022-07-17 06:36:25 +0600

    I wrote a Home Fragment and a local date for it

--- app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/home/HomeFragment.kt
+++ app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/home/HomeFragment.kt
@@ -1,15 +1,33 @@
 package com.geektech.beauty.presentation.ui.fragments.main.home
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.beauty.R
 import com.geektech.beauty.core.base.BaseFragment
 import com.geektech.beauty.databinding.FragmentHomeBinding
+import com.geektech.beauty.presentation.ui.adapters.ServiceCategoryAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
     override val binding by viewBinding(FragmentHomeBinding::bind)
     override val viewModel by viewModels<HomeViewModel>()
+    private val adapter = ServiceCategoryAdapter()
 
+    override fun initialize() {
+        binding.recyclerViewCategories.adapter = adapter
+    }
+
+    override fun launchObservers() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.categoryState.observe(this@HomeFragment) {
+                    adapter.submitList(it)
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/home/HomeViewModel.kt
+++ app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/home/HomeViewModel.kt
@@ -1,8 +1,38 @@
 package com.geektech.beauty.presentation.ui.fragments.main.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.geektech.beauty.core.base.BaseViewModel
+import com.geektech.beauty.presentation.models.CategoryUI
+import com.geektech.beauty.presentation.models.PopularUI
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : BaseViewModel()
+class HomeViewModel @Inject constructor() : BaseViewModel() {
+
+    private val _categoryState = MutableLiveData<List<CategoryUI>>()
+    val categoryState: LiveData<List<CategoryUI>> = _categoryState
+
+
+    private fun setLocalData() {
+        val list = ArrayList<CategoryUI>()
+        _categoryState.value = list
+        for (i in 1..20)
+            list.add(
+                CategoryUI(
+                    "https://static.onecms.io/wp-content/uploads/sites/23/2022/04/25/hair-wrapping-2000.jpg",
+                    "Услуги парикмахера"
+                )
+            )
+        _categoryState.value = list
+
+        val listPopular = ArrayList<PopularUI>()
+
+
+    }
+
+    init {
+        setLocalData()
+    }
+}
