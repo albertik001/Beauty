a0291782123211b34986cfe97d3bb5394ad0cdc4
Clar1fy <timplifier@gmail.com>
2022-07-18 03:03:24 +0600

    set up adapter onBind logic, handled onItemClick; built the mastery parameter on the ServiceCategorySpecialistUI.kt

--- app/src/main/java/com/geektech/beauty/presentation/models/ServiceCategorySpecialistUI.kt
+++ app/src/main/java/com/geektech/beauty/presentation/models/ServiceCategorySpecialistUI.kt
@@ -1,6 +1,7 @@
 package com.geektech.beauty.presentation.models
 
 data class ServiceCategorySpecialistUI(
+    val mastery: String,
     val specialistImage: String,
     val nameAndMasteryType: String,
     val yearsOfExperience: Int,
