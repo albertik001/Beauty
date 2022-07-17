d853c14de3cd2a452448bb156d72c9deb6b70f59
Clar1fy <timplifier@gmail.com>
2022-07-17 21:44:28 +0600

    feat(SpecialistProfileFragment): instantiated specialistProfileFragment,viewModel and layout

---
+++ app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/home/specialists/profile/SpecialistProfileFragment.kt
@@ -0,0 +1,15 @@
+package com.geektech.beauty.presentation.ui.fragments.main.home.specialists.profile
+
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.geektech.beauty.R
+import com.geektech.beauty.core.base.BaseFragment
+import com.geektech.beauty.databinding.FragmentSpecialistProfileBinding
+
+
+class SpecialistProfileFragment :
+    BaseFragment<FragmentSpecialistProfileBinding, SpecialistProfileViewModel>(R.layout.fragment_specialist_profile) {
+    override val binding by viewBinding(FragmentSpecialistProfileBinding::bind)
+    override val viewModel by viewModels<SpecialistProfileViewModel>()
+
+}
---
+++ app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/home/specialists/profile/SpecialistProfileViewModel.kt
@@ -0,0 +1,8 @@
+package com.geektech.beauty.presentation.ui.fragments.main.home.specialists.profile
+
+import com.geektech.beauty.core.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SpecialistProfileViewModel : BaseViewModel() {
+}
