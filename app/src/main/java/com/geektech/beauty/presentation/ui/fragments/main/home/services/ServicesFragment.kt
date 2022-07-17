bfecbcdd7198e38d2bbc7d1429bd7786c6e44311
Clar1fy <timplifier@gmail.com>
2022-07-17 22:03:00 +0600

    feat(drawable): imported back arrow

--- app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/home/services/ServicesFragment.kt
+++ app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/home/services/ServicesFragment.kt
@@ -1,10 +1,18 @@
 package com.geektech.beauty.presentation.ui.fragments.main.home.services
 
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.geektech.beauty.R
 import com.geektech.beauty.core.base.BaseFragment
 import com.geektech.beauty.databinding.FragmentServicesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 
-class ServicesFragment : BaseFragment<FragmentServicesBinding, ServicesViewModel>(R.layout.ser) {
+@AndroidEntryPoint
+class ServicesFragment :
+    BaseFragment<FragmentServicesBinding, ServicesViewModel>(R.layout.fragment_services) {
+    override val binding by viewBinding(FragmentServicesBinding::bind)
+    override val viewModel by viewModels<ServicesViewModel>()
+
 
 }
