12206f5ff18ec444e30a6ccde873a569f7629bfb
Clar1fy <timplifier@gmail.com>
2022-07-20 01:00:58 +0600

    created data class for detailedCategoryAdapter

--- app/src/main/java/com/geektech/beauty/presentation/models/DetailedCategoryUI.kt
+++ app/src/main/java/com/geektech/beauty/presentation/models/DetailedCategoryUI.kt
@@ -1,7 +1,7 @@
 package com.geektech.beauty.presentation.models
 
 data class DetailedCategoryUI(
-    val serviceImage: String,
+    val serviceImage: String? = null,
     val serviceType: String,
     val serviceTime: String,
     val serviceCost: String
