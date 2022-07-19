fa7861bc4dc7209e12e23cf1f82daba96107d559
Clar1fy <timplifier@gmail.com>
2022-07-20 03:12:16 +0600

    design

---
+++ app/src/main/java/com/geektech/beauty/presentation/ui/fragments/main/salon/services/specialists/appointment/AppointmentVerificationSucceededDialogFragment.kt
@@ -0,0 +1,20 @@
+package com.geektech.beauty.presentation.ui.fragments.main.salon.services.specialists.appointment
+
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.geektech.beauty.R
+import com.geektech.beauty.core.base.BaseDialogFragmentWithoutViewModel
+import com.geektech.beauty.databinding.FragmentAppointmentVerificationSucceededDialogBinding
+
+
+class AppointmentVerificationSucceededDialogFragment :
+    BaseDialogFragmentWithoutViewModel<FragmentAppointmentVerificationSucceededDialogBinding>(R.layout.fragment_appointment_verification_succeeded_dialog) {
+    override val binding by viewBinding(FragmentAppointmentVerificationSucceededDialogBinding::bind)
+    override fun setupListeners() {
+        binding.mcvOk.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+
+}
