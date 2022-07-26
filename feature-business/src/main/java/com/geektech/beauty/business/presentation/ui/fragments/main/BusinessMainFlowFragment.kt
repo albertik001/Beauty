99ce54cf622253f5a52fde7cfaa983a9edff835c
Clar1fy <timplifier@gmail.com>
2022-07-27 01:07:20 +0600

    BusinessMainFlowFragment for the main business logic of the business side

---
+++ feature-business/src/main/java/com/geektech/beauty/business/presentation/ui/fragments/main/BusinessMainFlowFragment.kt
@@ -0,0 +1,12 @@
+package com.geektech.beauty.business.presentation.ui.fragments.main
+
+import com.geektech.beauty.business.R
+import com.geektech.beauty.core.base.BaseFlowFragment
+
+
+class BusinessMainFlowFragment : BaseFlowFragment(
+    R.layout.fragment_business_main_flow,
+    R.id.nav_host_fragment_business_main_container
+) {
+
+}
