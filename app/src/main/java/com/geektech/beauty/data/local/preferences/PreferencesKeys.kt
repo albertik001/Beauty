68aae8b39f03b2a91d9990580c61f62565e11b35
Clar1fy <timplifier@gmail.com>
2022-07-27 01:05:08 +0600

    moved to core

--- app/src/main/java/com/geektech/beauty/data/local/preferences/PreferencesKeys.kt
+++
@@ -1,7 +0,0 @@
-package com.geektech.beauty.data.local.preferences
-
-object PreferencesKeys {
-    const val IS_AUTHENTICATED_KEY = "isAuthenticated"
-    const val IS_USER_A_CLIENT = "isUserAClient"
-    const val HAS_ONBOARD_BEEN_SEEN = "hasOnboardBeenSeen"
-}
