88226254ecd166a799d3996a32d531dfcc486375
Clar1fy <timplifier@gmail.com>
2022-07-26 17:38:56 +0600

    feat(Flow): building navigation flow

---
+++ feature-business/src/main/java/com/geektech/beauty/business/presentation/ui/fragments/auth/BusinessAuthenticationFlowFragment.kt
@@ -0,0 +1,14 @@
+package com.geektech.beauty.business.presentation.ui.fragments.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.geektech.beauty.business.R
+import com.geektech.beauty.core.base.BaseFlowFragment
+
+
+class BusinessAuthenticationFlowFragment : BaseFlowFragment() {
+
+}
